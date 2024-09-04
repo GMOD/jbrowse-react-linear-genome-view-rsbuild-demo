6fb0c4d09dce2562bef8f4a294515fb5cfe3d5b8
Colin <colin.diesh@gmail.com>
2024-09-03 22:26:59 -0400

    Misc

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
   plugins: [pluginReact()],
+  output: {
+    assetPrefix: "auto",
+  },
+  html: {
+    title: "@jbrowse/react-linear-genome-view rsbuild demo",
+  },
 });
